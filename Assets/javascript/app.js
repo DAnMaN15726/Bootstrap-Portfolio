774375568ee63ac5ab2353f8984b1c8292a3fe98
danman15726 <danman15726@gmail.com>
2019-11-19 00:02:36 -0600

    Added lines in the app.js file that will launch the repository and deployed link in 2 seperate tabs.

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,6 +1,9 @@
 $(".card").on("click", function(){
+    console.log("CARD-CLICK ACTIVATED");
+    // window.location = "https://github.com/haleyserr/Project-1";
+    // window.location = "https://haleyserr.github.io/Project-1/";
 
-    
-
+    window.open("https://github.com/haleyserr/Project-1");
+    window.open("https://haleyserr.github.io/Project-1/");
 
 });
