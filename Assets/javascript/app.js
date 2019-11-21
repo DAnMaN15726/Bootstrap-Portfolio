6df6c30320d323b2cb023d6cd53cc6da91c18c90
danman15726 <danman15726@gmail.com>
2019-11-20 22:46:04 -0600

    Added final styling for now. Couldn't get the drop-down button to work. Finished updating everything.

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,4 +1,4 @@
-$(".card").on("click", function(){
+$("#task").on("click", function(){
     console.log("CARD-CLICK ACTIVATED");
     // window.location = "https://github.com/haleyserr/Project-1";
     // window.location = "https://haleyserr.github.io/Project-1/";
@@ -6,4 +6,12 @@ $(".card").on("click", function(){
     window.open("https://github.com/haleyserr/Project-1");
     window.open("https://haleyserr.github.io/Project-1/");
 
+});
+
+
+
+$(".navbar-toggler").on("click", function(){
+    console.log("CLICK REGISTERED");
+    
+    $('.dropdown-menu').addClass('open');
 });
