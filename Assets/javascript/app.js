ca1742d8f29861cd83b24666b5b3a75f22efb295
danman15726 <danman15726@gmail.com>
2019-12-16 00:01:32 -0600

    added the Bamazon node app to the portfolio as well as a click function that launches the user directly to the corresponding git repository in a seperate open window.

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -12,9 +12,7 @@ $("#task").on("click", function(){
 
 $("#liri").on("click", function(){
     console.log("CARD-CLICK ACTIVATED");
-    // window.location = "https://github.com/haleyserr/Project-1";
-    // window.location = "https://haleyserr.github.io/Project-1/";
-
+    
     window.open("https://github.com/DAnMaN15726/liri-node-app");
     
 
@@ -26,4 +24,14 @@ $(".navbar-toggler").on("click", function(){
     console.log("CLICK REGISTERED");
     
     $('.dropdown-menu').addClass('open');
+});
+
+
+
+$("#Bamazon").on("click", function(){
+    console.log("CARD-CLICK ACTIVATED");
+    
+    window.open("https://github.com/DAnMaN15726/BAmazon");
+    
+
 });
