e2372b28e1a65c16a56ba912da993d7d924ab8ea
danman15726 <danman15726@gmail.com>
2020-02-05 20:45:11 -0600

    Converting to a node app, just because I can. Rerouting everything into a pseudo MVP style directory.

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const exphbs = require("express-handlebars");
+
+const routes = require("./routes/controller.js");
+
+
+const app = express();
+
+
+const PORT = process.env.PORT || 2222;
+
+app.use(express.static("./public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log(`APP LISTENING: ${PORT}`);
+});
+
+
+
+
