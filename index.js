eb22e45ff432415ce64367cb1477ed8b9c1ce385
danman15726 <danman15726@gmail.com>
2020-02-05 21:29:47 -0600

    Finished converting to handlebars/node/etc. . Made the mistake of not git ignoring node. so they are all uploaded in that git repo.

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const express = require("express");
 
 const exphbs = require("express-handlebars");
-
 const routes = require("./routes/controller.js");
-
-
 const app = express();
 
 
 const PORT = process.env.PORT || 2222;
 
 app.use(express.static("./public"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(routes);
 
 
@@ -22,10 +17,6 @@ app.use(routes);
 
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
-
-
-
-
 
 
 
